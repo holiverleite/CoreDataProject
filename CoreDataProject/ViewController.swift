660bdbd49777cfcd6068375b49ce3cfd41d1b1c4
Haroldo Oliveira de Almeida Leite <holiverleite@gmail.com>
2019-08-21 15:24:43 -0300

    -Implementing tableview delegate.

--- CoreDataProject/ViewController.swift
+++ CoreDataProject/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            self.tableView.dataSource = self
+            self.tableView.delegate = self
+        }
+    }
+    
+    @IBAction func addName(_ sender: Any) {
+        
+    }
+    
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.title = "The list"
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
 }
 
