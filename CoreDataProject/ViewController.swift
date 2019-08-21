1358f6b2d02b3435b775c0948b6bf1951e66862f
Haroldo Oliveira de Almeida Leite <holiverleite@gmail.com>
2019-08-21 14:36:55 -0300

    -Initial commit; -Implementing a screen with a table view to add and remove objects from database;

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by monitora on 21/08/19.
+//  Copyright © 2019 Haroldo Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
